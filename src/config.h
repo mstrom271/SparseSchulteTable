be5b8aeb17361331d3fa28847cf2dbceda35e1aa
Vitaliy Lytonin <mstrom2@rambler.ru>
2024-04-02 09:19:57 +0400

    port changes from crossplatform template project

---
+++ src/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// clang-format off
+#define PROJECT_NAME "SparseSchulte"
+#define PROJECT_VERSION "0.5"
+#define PROJECT_VERSION_MAJOR "0"
+#define PROJECT_VERSION_MINOR "5"
+#define ORGANIZATION_NAME "TapMyApp"
+// clang-format on
