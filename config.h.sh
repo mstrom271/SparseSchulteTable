beb3d37cb4365bc72b49f2d65362c23e460f52a0
Vitaliy Lytonin <mstrom2@rambler.ru>
2024-04-07 13:07:08 +0400

    fix BUILD_NUMBER incremet under windows

--- config.h.sh
+++ config.h.sh
@@ -3,7 +3,7 @@
 FILE="src/config.h"
 
 # increament BUILD_NUMBER
-CURRENT_VALUE=$(grep -oP "BUILD_NUMBER\s+\K\d+" "$FILE")
+CURRENT_VALUE=$(awk "/BUILD_NUMBER/{print \$3}" "$FILE")
 NEW_VALUE=$((CURRENT_VALUE + 1))
 sed -i "s/BUILD_NUMBER\s\+[0-9]\+/BUILD_NUMBER ${NEW_VALUE}/g" "$FILE"
 
